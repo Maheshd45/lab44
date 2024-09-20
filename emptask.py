ae55e74e8d69127334b0be25fd590a47b1660542
Maheshd45 <maheshdasari2k21@gmail.com>
2024-09-20 11:55:55 +0530

    Add files via upload

---
+++ emptask.py
@@ -0,0 +1,14 @@
+emp = {'eno':[1, 2, 3], 'ename':['A', 'B', 'C'], 'esal':[10000, 22000, 30000]}
+print("\n the employee dataset is:")
+print(emp)
+print("------------------------------------------")
+print("\n The Employee Name are:",emp['ename'])
+print("------------------------------------------")
+print("\n the employee salaries are:")
+print("------------------------------------------")
+for i in emp['esal']:
+    print(i)
+
+# sample task : you have to implement
+# i am making changes
+    
